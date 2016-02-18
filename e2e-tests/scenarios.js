7273b5d07f95eb8aa93094bdc8a9c7cf51ac3efa
arodang <sfbattlefield2@gmail.com>
2016-02-17 20:06:59 -0500

    updated protractor test for /village as default route

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,8 +7,8 @@ describe('my app', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /village when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/village");
   });
 
 
