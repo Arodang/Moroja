40234ebb63ba91eb0573285d260fe2926f41ddbb
Hones <SeeD_Garden@hotmail.com>
2016-03-06 12:06:54 -0500

    Fixed refresh button, used hacky page refresh.

--- app/js/structure/header/directives/headerDirective.js
+++ app/js/structure/header/directives/headerDirective.js
@@ -2,7 +2,21 @@
 module.exports = function () {
     return {
         templateUrl : 'js/structure/header/directives/header.html',
-        controller: function($scope, StorageService) {;
+        controller: function($scope, StorageService)
+        {
+            $scope.resetStorage=function()
+            {
+                StorageService.resetVillage();
+                /*
+                1.refresh page manually
+                2.callbacks
+                3.events
+                 */
+                //quickfix
+                location.reload();
+            };
+
         }
+
     };
 };
